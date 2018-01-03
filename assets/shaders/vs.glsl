7f26a2cab4addb9da7625bb401dd9a751b223eef
Lucas <lucas.dussouchaud@gmail.com>
2018-01-03 16:26:04 +0100

    clear

--- assets/shaders/vs.glsl
+++
@@ -1,9 +0,0 @@
-#version 330 core
-layout (location = 0) in vec2 vertex;
-
-uniform mat4 model;
-uniform mat4 projection;
-
-void main () {
-    gl_Position = projection * model * vec2(vertex.xy);
-}
